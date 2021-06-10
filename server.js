a50638767052204fe35330794ce3554f99a09611
Armin <arminch1997@gmail.com>
2021-06-09 19:50:44 -0700

    code

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const { join } = require('path')
+let db = require('./db/db.json')
 
 app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
@@ -9,4 +10,15 @@ app.listen(process.env.PORT || 3000)
 
 app.get('/notes', (req,res) =>{
   res.sendFile(join(__dirname, './public/notes.html'))
+})
+app.get('/api/notes', (req,res)=>{
+  res.json(db)
+})
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, './public/index.html'))
+})
+app.post('/api/notes', (req, res) => {
+  db.push({'title': req.body.title, 'text': req.body.text})
+  res.json(db)
+  console.log(req.body)
 })
